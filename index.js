87ed4b24988733b67e6afa79bce427f4178905da
Ecort-hue <joshuacortes017@gmail.com>
2026-03-18 12:20:48 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
+const viewButton = document.querySelector('.view')
 const checkButton = document.querySelector('.Check');
 
 checkButton.addEventListener('click', () => {
     window.location.href = 'portfolio.html';
 });
+
+
+
+anime({
+    targets: 'h1',
+    translateY: [-30, 0],
+    opacity: [0, 2],
+    duration: 800,
+    eases: 'easeOutQuad'
+});
+
+
+anime({
+    targets: '.block',
+    translateY: [60, 0],
+    opacity: [0, 1],
+    duration: 900,
+    delay: 300,
+    easing: 'easeOutQuad'
+});
+
+
+anime({
+    targets: '.circle',
+    translateY: [0,-25,0],
+    easing: 'spring',
+    stiffness: 150, damping: 5,
+    loop:true,
+});
+
+anime({
+    targets: '.text-xl',
+    lines:true,
+    class: 'split-word',
+    translateY: ['75%', '0%'],
+    duration: 750,
+    ease: 'inOutQuad',
+    loop: true,
+    
+})
--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,19 @@
-const popover = new bootstrap.Popover('.popover-dismiss', {
-    trigger: 'focus'
-})
+
+
+anime({
+    targets: '.navbar',
+    translateY: [-50, 0],
+    opacity: [0, 1],
+    duration: 1000,
+    eases: 'OutQuad'
+});
+
+
+
+
+const draggable =
+anime.createDraggable('.card', {
+    container: '.grid',
+    snap: 56, 
+    x: { snap: [0, 200] }, 
+});
