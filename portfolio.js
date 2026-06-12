088496fdd66c3932660af70327cfbfb25e0f2f32
Ecort-hue <joshuacortes017@gmail.com>
2026-06-11 20:23:54 -0400

    A revised version thats finished
    reworked showcase UI

--- portfolio.js
+++ portfolio.js
@@ -1,19 +1,30 @@
+// portfolio.js
 
-
+// Navbar entrance
 anime({
     targets: '.navbar',
     translateY: [-50, 0],
     opacity: [0, 1],
-    duration: 1000,
-    eases: 'OutQuad'
+    duration: 800,
+    easing: 'easeOutQuad'
 });
 
+// Hero entrance
+anime({
+    targets: '.hero-section',
+    translateY: [30, 0],
+    opacity: [0, 1],
+    duration: 900,
+    delay: 200,
+    easing: 'easeOutQuad'
+});
 
-
-
-const draggable =
-anime.createDraggable('.card', {
-    container: '.grid',
-    snap: 56, 
-    x: { snap: [0, 200] }, 
+// Cards staggered entrance
+anime({
+    targets: '.project-card',
+    translateY: [50, 0],
+    opacity: [0, 1],
+    duration: 700,
+    delay: anime.stagger(100, { start: 400 }),
+    easing: 'easeOutQuad'
 });
